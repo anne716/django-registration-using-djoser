562485278770a1e9a1bd537ccc73afd78d642737
Anna Nicole Gabriento <100785169+peanut150@users.noreply.github.com>
2023-04-09 19:44:25 +0800

    Uploading React Files and Folders

---
+++ react-project/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './pages/authentication/registration';
+
+function App() {
+  return (
+    <Register />
+  );
+}
+
+export default App;
